82062d55a90dd9b77c7f3d1c04a9c2cd8851b0a0
roble <renanroble@gmail.com>
2025-12-27 22:18:48 +0000

    refactor: update AuthServiceProvider to use Spatie Navigation facade and remove redundant label from logout navigation

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace Modules\Auth\Providers;
 
-use App\Facades\Navigation;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
+use Spatie\Navigation\Facades\Navigation;
 use Spatie\Navigation\Section;
 
 class AuthServiceProvider extends ServiceProvider
@@ -41,7 +41,6 @@ class AuthServiceProvider extends ServiceProvider
         Navigation::add('Log out', '#', function (Section $section) {
             $section->attributes([
                 'group' => 'user',
-                'label' => 'Log out',
                 'action' => 'logout',
                 'icon' => 'lucide:log-out',
                 'order' => 100,
